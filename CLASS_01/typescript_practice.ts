14adbe72eddf3be77b45457a008cce9d60b05590
Sk Nayeem <sknayeemislam384@gmail.com>
2025-07-22 00:05:51 +0600

    TypeScript Basics Practice

---
+++ CLASS_01/typescript_practice.ts
@@ -0,0 +1,9 @@
+// TypeScript Basics Practice
+
+// 1. Primitive Types
+let userName: string = "Nayeem";
+let age: number = 22;
+let isActive: boolean = true;
+let userScore: number | null = null;
+
+// 2. Object Types
